fc7b212c54384e1a1b492c584fd7c6e2564d46ea
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-23 09:41:09 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Leo  on 23.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
